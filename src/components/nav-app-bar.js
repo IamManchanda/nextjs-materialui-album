923792f277fe35d04af3b6471c4cdee4691863e9
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-19 21:10:18 +0530

    Nav App bar icon component

--- src/components/nav-app-bar.js
+++ src/components/nav-app-bar.js
@@ -1,11 +1,19 @@
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import { PhotoCamera } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+}));
 
 function NavAppBar() {
+  const classes = useStyles();
   return (
     <AppBar position="relative" color="primary">
       <Toolbar>
-        <PhotoCamera />
+        <PhotoCamera className={classes.icon} />
         <Typography variant="h6" color="inherit" noWrap>
           Photo Album
         </Typography>
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,6 @@ const useStyles = makeStyles((theme) => ({
 
 function PageIndex() {
   const classes = useStyles();
-
   return (
     <Fragment>
       <Head>
